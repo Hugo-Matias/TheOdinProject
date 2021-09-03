8cc24b28d1eba13968d89bdb0c7f2f83c679bbf8
Rogerooo <hugo.lopesmatias@gmail.com>
2021-09-03 22:37:11 +0100

    Remove unnecessary resources from previous tutorial

--- Webpack-Demo/src/index.js
+++ Webpack-Demo/src/index.js
@@ -1,21 +1,5 @@
 import _ from "lodash";
 import myName from "./myName";
-import "./style.css";
-import Icon from "./icon.jpg";
-import Data from "./data.xml";
-import Notes from "./data.csv";
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json5 from './data.json5';
-
-console.log(toml.title);
-console.log(toml.owner.name);
-
-console.log(yaml.title);
-console.log(yaml.owner.name);
-
-console.log(json5.title);
-console.log(json5.owner.name);
 
 function component() {
   const element = document.createElement("div");
@@ -26,16 +10,6 @@ function component() {
 
   // Using our function instead according to Odin Project tutorial
   element.innerHTML = myName("Cody");
-  element.classList.add("hello");
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
