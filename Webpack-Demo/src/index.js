835d43ca9685c29aa29de592fa845c49bec47e02
Rogerooo <hugo.lopesmatias@gmail.com>
2021-09-10 18:11:56 +0100

    Complete Webpack Bundle management tutorial

--- Webpack-Demo/src/index.js
+++ Webpack-Demo/src/index.js
@@ -1,8 +1,10 @@
 import _ from "lodash";
 import myName from "./myName";
+import printMe from "./print";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   // Lodash, previously  included via a script, is required for this line to work
   // Lodash is now imported by this script.
@@ -10,6 +12,11 @@ function component() {
 
   // Using our function instead according to Odin Project tutorial
   element.innerHTML = myName("Cody");
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
